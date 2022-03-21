99e8a95ebb15d194a0cf60fd29cf7784f0c3e3d3
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-03-21 16:36:16 +0000

    Update namespacing

--- src/gateways/Gateway.php
+++ src/gateways/Gateway.php
@@ -221,7 +221,6 @@ class Gateway extends OffsiteGateway
         $view->setTemplateMode(View::TEMPLATE_MODE_CP);
 
         $html = $view->renderTemplate('commerce-mollie/paymentForm', $params);
-        $html = Html::namespaceInputs($html, sprintf('%s[%s]', PaymentsController::PAYMENT_FORM_NAMESPACE, $this->handle));
 
         $view->setTemplateMode($previousMode);
 
